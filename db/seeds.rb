098a8195565fbc8ce7333919b7d8a77436a40b4c
vovchif17 <vovchif17@gmail.com>
2021-05-11 22:36:24 +0200

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+Product.delete_all
+Product.create!(title: 'Build',
+description:
+%(<p>
+  <em>Responsive,Intuitive Inter faces with Ruby</em>
+  The nextstepin the evolution of userinter facesis here
+  </p>),
+image_url: 'dpchat.jpg',
+price: 20.00)
